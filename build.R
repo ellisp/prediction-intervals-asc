0b1c500ec4c5e2047f46056ae8d93a05d5274c94
Peter Ellis <peter.ellis2013nz@gmail.com>
2016-10-15 15:37:33 +1300

    basic build.R script

--- build.R
+++ build.R
@@ -1,2 +1,9 @@
 library(knitr)
 library(rmarkdown)
+
+source("analysis/helper-functions.R")
+source("analysis/dodgy-pi.R")
+source("analysis/prediction-intervals-coverage.R") # takes 20+ minutes to run
+source("analysis/point-accuracy.R")
+
+# rendering the presentation easiest done with the knit button
